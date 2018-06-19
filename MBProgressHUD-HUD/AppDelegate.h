a1c3b67f8b71b7f897da27eebac29a0dadcfeae1
chenyang <1525238065@qq.com>
2018-06-19 15:40:03 +0800

    Initial Commit

---
+++ MBProgressHUD-HUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MBProgressHUD-HUD
+//
+//  Created by careland on 2018/6/19.
+//  Copyright © 2018年 careland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MBProgressHUD-HUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MBProgressHUD-HUD
+//
+//  Created by careland on 2018/6/19.
+//  Copyright © 2018年 careland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
